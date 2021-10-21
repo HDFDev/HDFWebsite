5ec8d9b46b3d8b713bc5270161779964f89b6da5
Varun Dutia <varundutia.h@gmail.com>
2021-10-21 11:52:36 +0530

    Changed settings for Cpanel

--- contact.php
+++ contact.php
@@ -118,7 +118,7 @@
                 </div>
                 <div class="contact-form">
                         <div id="success"></div>
-                        <form method="POST" name="sentMessage" id="contactForm" novalidate="novalidate" action= "<?php echo$_SERVER['PHP_SELF'];?>">
+                        <form method="POST" name="sentMessage" id="contactForm">
                             <div class="control-group">
                                 <input type="text" class="form-control" name="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
                                 <p class="help-block text-danger"></p>
@@ -136,7 +136,7 @@
                                 <p class="help-block text-danger"></p>
                             </div>
                             <div>
-                                <button class="btn btn-custom" type="submit" name="sendMessageButton">Send Message</button>
+                                <button class="btn btn-custom" type="submit" name="submit">Send Message</button>
                             </div>
                         </form>
                     </div>
@@ -256,37 +256,39 @@
 </body>
 </html>
 <?php
-  if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    echo "<script type='text/javascript'>
-    error();
-    </script>";
-    exit();
-  }
-  
-  $name = strip_tags(htmlspecialchars($_POST['name']));
-  $email = strip_tags(htmlspecialchars($_POST['email']));
-  $subject = strip_tags(htmlspecialchars($_POST['subject']));
-  $message = strip_tags(htmlspecialchars($_POST['message']));
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "healingd_queries";
+if(isset($_POST['submit'])){
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "insert into Queries(Name, Email, Subject, Message) values ('$name', '$email', '$subject','$message')";
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "<script type='text/javascript'>
-  success();
-  </script>" ;
-} catch(PDOException $e) {
-  echo "<script type='text/javascript'>
-  error();
-  </script>";
+    if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        echo "<script type='text/javascript'>
+        error();
+        </script>";
+        exit();
+      }
+      $name = strip_tags(htmlspecialchars($_POST['name']));
+      $email = strip_tags(htmlspecialchars($_POST['email']));
+      $subject = strip_tags(htmlspecialchars($_POST['subject']));
+      $message = strip_tags(htmlspecialchars($_POST['message']));
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $dbname = "healingd_queries";
+    
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "insert into Queries(Name, Email, Subject, Message) values ('$name', '$email', '$subject','$message')";
+      // use exec() because no results are returned
+      $conn->exec($sql);
+      echo "<script type='text/javascript'>
+      success();
+      </script>" ;
+    } catch(PDOException $e) {
+      echo "<script type='text/javascript'>
+      error();
+      </script>";
+    }
+
 }
-
 $conn = null;   
 ?>
