6dd9687bcb40425048a695bbc37bd9ad799663ca
Varun Dutia <varundutia.h@gmail.com>
2021-10-24 21:36:56 +0530

    Mailer

--- contact.php
+++ contact.php
@@ -270,8 +270,10 @@ if(isset($_POST['submit'])){
       $subject = strip_tags(htmlspecialchars($_POST['subject']));
       $message = strip_tags(htmlspecialchars($_POST['message']));
       $servername = "localhost";
-      $username = "healingd_database";
-      $password = "HDFData@123";
+    //   $username = "healingd_database";
+    //   $password = "HDFData@123";
+    $username = "root";
+    $password = "Test$123";
       $dbname = "healingd_queries";
     
     try {
@@ -284,38 +286,9 @@ if(isset($_POST['submit'])){
       echo "<script type='text/javascript'>
       success();
       </script>" ;
-    $mail = new PHPMailer;
-
-    // $mail->SMTPDebug = 4;                               // Enable verbose debug output
-
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = "developer.hdf1@gmail.com";                 // SMTP username
-    $mail->Password = "Hdove_21Dev";                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
-
-    $mail->setFrom(EMAIL, 'Dsmart Tutorials');
-    $mail->addAddress($_POST['email']);     // Add a recipient
-
-    $mail->addReplyTo(EMAIL);
-    // print_r($_FILES['file']); exit;
-    for ($i=0; $i < count($_FILES['file']['tmp_name']) ; $i++) { 
-        $mail->addAttachment($_FILES['file']['tmp_name'][$i], $_FILES['file']['name'][$i]);    // Optional name
-    }
-    $mail->isHTML(true);                                  // Set email format to HTML
-
-    $mail->Subject = $_POST['subject'];
-    $mail->Body    = '<div style="border:2px solid red;">This is the HTML message body <b>in bold!</b></div>';
-    $mail->AltBody = $_POST['message'];
-
-    if(!$mail->send()) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
-        echo 'Message has been sent';
-    }
+      $msg = "First line of text\nSecond line of text";
+      mail("mailer@healingdovefoundation.org","My subject",$msg);
+   
     } catch(PDOException $e) {
         echo $e;
       echo "<script type='text/javascript'>
